3018d639919b81820a12386d9ee832adef79a95b
danigb <danigb@gmail.com>
2010-01-05 12:44:19 +0100

    backups

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,4 @@
 # Be sure to restart your server when you modify this file
-
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
@@ -7,7 +6,9 @@ RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem 'ferret'
   config.gem 'acts_as_ferret'
+  
   config.action_controller.page_cache_directory = RAILS_ROOT + "/public/cache/"
   config.time_zone = 'UTC'
 end
